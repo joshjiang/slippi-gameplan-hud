35a00fb019ceb1fe7a48a2e9010f22af98521130
Josh Jiang <josh.jiang.dbf@gmail.com>
2020-11-26 18:43:36 -0500

    POC - Got character ID from payload

--- index.js
+++ index.js
@@ -1,61 +1,34 @@
-/*
-This script reads live slp files from a folder, and writes
-the players stock count and percentages to a file.
-*/
-
-// TODO: Set this folder!!
-const playerInfoFolder = "/mnt/c/Users/User/Desktop";
 
 const { SlpFolderStream, SlpRealTime } = require("@vinceau/slp-realtime");
-const fs = require("fs");
-const path = require("path");
+const {Character} = require("@slippi/slippi-js");
 
-// TODO: Make sure you set this value!
 const slpLiveFolderPath = "/mnt/c/Users/User/Documents/Slippi";
+
 console.log(`Monitoring ${slpLiveFolderPath} for new SLP files`);
 
-// Set up the handlers
-
-const errHandler = (err) => {
-  if (err) {
-    console.error(err);
-  }
-};
-
-const setPlayerStock = (player, stock) => {
-  fs.writeFile(path.join(playerInfoFolder, `player${player}Stocks.txt`), stock, errHandler);
-};
-
 const setPlayerPercent = (player, percent) => {
-  fs.writeFile(path.join(playerInfoFolder, `player${player}Percent.txt`), percent, errHandler);
+  console.log(`player${player}Percent: ${percent}`);
 };
 
 // Connect to the relay
 const stream = new SlpFolderStream();
 const realtime = new SlpRealTime();
 realtime.setStream(stream);
-realtime.game.start$.subscribe(() => {
+
+// Get character
+realtime.game.start$.subscribe((payload) => {
   console.log(`Detected a new game in ${stream.latestFile()}`);
+  console.log(Character[payload.players[0].characterId])
+  console.log(Character[payload.players[1].characterId])
 });
+
 realtime.stock.percentChange$.subscribe((payload) => {
   const player = payload.playerIndex + 1;
   console.log(`player ${player} percent: ${payload.percent}`);
   setPlayerPercent(player, `${Math.floor(payload.percent)}%`);
 });
 
-realtime.stock.countChange$.subscribe((payload) => {
-  const player = payload.playerIndex + 1;
-  console.log(`player ${player} stocks: ${payload.stocksRemaining}`);
-  setPlayerStock(player, payload.stocksRemaining.toString());
-});
 
-// Reset the text files on game end
-realtime.game.end$.subscribe(() => {
-  setPlayerStock(1, "");
-  setPlayerPercent(1, "");
-  setPlayerStock(2, "");
-  setPlayerPercent(2, "");
-});
 
 // Start monitoring the folder for changes
 stream.start(slpLiveFolderPath);
